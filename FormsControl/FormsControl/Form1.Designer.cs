41b2597eba02988b53ecf9b626f9788a0d39c15f
guven.bilgin <gvnblgn@hotmail.com>
2017-07-20 21:25:49 +0300

    terminated ok .

---
+++ FormsControl/FormsControl/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace FormsControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtBir = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(0, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 18);
+            this.lbl1.TabIndex = 0;
+            // 
+            // txtBir
+            // 
+            this.txtBir.Location = new System.Drawing.Point(12, 33);
+            this.txtBir.Name = "txtBir";
+            this.txtBir.Size = new System.Drawing.Size(229, 20);
+            this.txtBir.TabIndex = 1;
+            this.txtBir.TextChanged += new System.EventHandler(this.txtBir_TextChanged);
+            this.txtBir.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.txtBir_MouseDoubleClick);
+            this.txtBir.MouseEnter += new System.EventHandler(this.txtBir_MouseEnter);
+            this.txtBir.MouseLeave += new System.EventHandler(this.txtBir_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 357);
+            this.Controls.Add(this.txtBir);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtBir;
+    }
+}
+
---
+++ FormsControl/FormsControl/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtBir_TextChanged(object sender, EventArgs e)
+        {
+            lbl1.Text = txtBir.Text;
+        }
+
+        private void txtBir_MouseEnter(object sender, EventArgs e)
+        {
+            txtBir.BackColor = Color.Orange;
+            txtBir.ForeColor = Color.Maroon;
+        }
+
+        private void txtBir_MouseLeave(object sender, EventArgs e)
+        {
+            txtBir.BackColor = Color.White;
+            txtBir.ForeColor = Color.Black;
+        }
+
+        private void txtBir_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("you double clicked to the box");
+        }
+    }
+}
