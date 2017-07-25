c9bb2ab9147ceac7fab15eb7c18a42dd5fbfeabb
guven.bilgin <gvnblgn@hotmail.com>
2017-07-25 21:11:15 +0300

    try catch finally

---
+++ tryCash/tryCash/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace tryCash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtDeger = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnDeger = new System.Windows.Forms.Button();
+            this.btnKontrol = new System.Windows.Forms.Button();
+            this.btnFinal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDeger
+            // 
+            this.txtDeger.Location = new System.Drawing.Point(29, 12);
+            this.txtDeger.Name = "txtDeger";
+            this.txtDeger.Size = new System.Drawing.Size(422, 20);
+            this.txtDeger.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnDeger
+            // 
+            this.btnDeger.Location = new System.Drawing.Point(29, 52);
+            this.btnDeger.Name = "btnDeger";
+            this.btnDeger.Size = new System.Drawing.Size(422, 23);
+            this.btnDeger.TabIndex = 1;
+            this.btnDeger.Text = "Hesapla";
+            this.btnDeger.UseVisualStyleBackColor = true;
+            this.btnDeger.Click += new System.EventHandler(this.btnDeger_Click);
+            // 
+            // btnKontrol
+            // 
+            this.btnKontrol.Location = new System.Drawing.Point(29, 90);
+            this.btnKontrol.Name = "btnKontrol";
+            this.btnKontrol.Size = new System.Drawing.Size(422, 23);
+            this.btnKontrol.TabIndex = 2;
+            this.btnKontrol.Text = "Kontrollü Onay";
+            this.btnKontrol.UseVisualStyleBackColor = true;
+            this.btnKontrol.Click += new System.EventHandler(this.btnKontrol_Click);
+            // 
+            // btnFinal
+            // 
+            this.btnFinal.Location = new System.Drawing.Point(29, 129);
+            this.btnFinal.Name = "btnFinal";
+            this.btnFinal.Size = new System.Drawing.Size(422, 23);
+            this.btnFinal.TabIndex = 3;
+            this.btnFinal.Text = "Final Kontrollü Onay";
+            this.btnFinal.UseVisualStyleBackColor = true;
+            this.btnFinal.Click += new System.EventHandler(this.btnFinal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 261);
+            this.Controls.Add(this.btnFinal);
+            this.Controls.Add(this.btnKontrol);
+            this.Controls.Add(this.btnDeger);
+            this.Controls.Add(this.txtDeger);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDeger;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnDeger;
+        private System.Windows.Forms.Button btnKontrol;
+        private System.Windows.Forms.Button btnFinal;
+    }
+}
+
---
+++ tryCash/tryCash/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tryCash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDeger_Click(object sender, EventArgs e)
+        {
+            long gelenDeger = long.Parse(txtDeger.Text);
+            //long gelenDeger = Convert.ToInt64(txtDeger.Text);
+
+            MessageBox.Show("Tebrikler, Iphone 8s plus kazandınız!");
+        }
+
+
+        /* En sol gri şeritte bir satır belirlenerek break point koyuyoruz.
+         * programı çalıştırdıktan sonra F10 tuşuyla adım adım ilerleyebiliyorsun.
+         */
+
+        private void btnKontrol_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Hata riski olan kodların yazıldığı alan
+                long gelenDeger = Convert.ToInt64(txtDeger.Text);
+                MessageBox.Show("Tebrikler, Iphone 8s plus kazandınız!");
+
+
+            }
+            catch (Exception hata)
+            {
+                //Hatalı işlem yapıldığında çalışan kod
+                //MessageBox.Show("Bir telefon numarası bile giremiyosun !! ");
+
+                MessageBox.Show(hata.Message);
+
+            }
+        }
+
+        private void btnFinal_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                long gelenDeger = Convert.ToInt64(txtDeger.Text);
+                MessageBox.Show("Tebrikler Iphone 8s plus kazandınız!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Hatasız kul olmaz, ben her türlü çalışırım ! ");
+            }
+        }
+    }
+}
