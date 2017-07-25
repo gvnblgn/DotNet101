ba06a9b61325e40a1997528d4ed2c73b47b2edc6
guven.bilgin <gvnblgn@hotmail.com>
2017-07-25 21:42:21 +0300

    end.

---
+++ ifElse/ifElse/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ifElse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.txtToplam = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(0, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(33, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Sayı1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(0, 24);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(33, 13);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Sayi2";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(66, -3);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi1.TabIndex = 2;
+            this.txtSayi1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSayi1_KeyDown);
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(66, 24);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi2.TabIndex = 3;
+            this.txtSayi2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSayi2_KeyDown);
+            // 
+            // txtToplam
+            // 
+            this.txtToplam.Location = new System.Drawing.Point(66, 50);
+            this.txtToplam.Name = "txtToplam";
+            this.txtToplam.Size = new System.Drawing.Size(100, 20);
+            this.txtToplam.TabIndex = 4;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(66, 93);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(100, 23);
+            this.btnHesapla.TabIndex = 5;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 354);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtToplam);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.TextBox txtToplam;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ ifElse/ifElse/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ifElse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(txtSayi1.Text);
+            int sayi2 = Convert.ToInt32(txtSayi2.Text);
+            int toplam = (sayi1 + sayi2) / 2 ;
+
+            if (toplam<100)
+            {
+                txtToplam.Text = toplam.ToString();
+                MessageBox.Show("Dönem ortalamanız : " + toplam);
+
+            }
+            else
+            {
+                MessageBox.Show("Girdiğiniz değer geçersizdir!!!");
+            }
+
+            txtSayi1.Clear();
+            txtSayi2.Clear();
+            txtToplam.Clear();
+            txtSayi1.Focus();
+
+            /*if (txtSayi1.Text.ToLower() == "admin") ; //toTrim yaparsan hem küçültür hem boşlukları siler
+            {
+                MessageBox.Show("Hoşgeldin admin");
+            }
+            else
+	        {
+                MessageBox.Show("Tanıyamadım kimsin?");
+            }
+            */
+        }
+
+        private void txtSayi1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //btnHesapla_Click(sender, e);
+                txtSayi2.Focus();
+            }
+            else
+            {
+                //this.Text == txtSayi1.Text;
+            }
+        }
+
+        private void txtSayi2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnHesapla_Click(sender, e);
+                txtSayi1.Focus();
+            }
+        }
+    }
+}
