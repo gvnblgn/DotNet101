b0daf52c6df079ed67cea5534859687f06f69f33
guven.bilgin <gvnblgn@hotmail.com>
2017-07-26 21:18:08 +0300

    listbox

---
+++ switchCase/switchCase/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace switchCase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbMevsim = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cbMevsim
+            // 
+            this.cbMevsim.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMevsim.FormattingEnabled = true;
+            this.cbMevsim.Location = new System.Drawing.Point(0, 0);
+            this.cbMevsim.Name = "cbMevsim";
+            this.cbMevsim.Size = new System.Drawing.Size(208, 21);
+            this.cbMevsim.TabIndex = 0;
+            this.cbMevsim.SelectedIndexChanged += new System.EventHandler(this.cbMevsim_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.cbMevsim);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbMevsim;
+    }
+}
+
---
+++ switchCase/switchCase/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace switchCase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            cbMevsim.Items.Add("Kış");
+            cbMevsim.Items.Add("İlkbahar");
+            cbMevsim.Items.Add("Yaz");
+            cbMevsim.Items.Add("Sonbahar");
+            
+        }
+
+        private void cbMevsim_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //1.Yol
+            string mevsim = cbMevsim.SelectedItem.ToString();
+            MessageBox.Show(mevsim);
+
+            //2.YOL
+            int index = cbMevsim.SelectedIndex;
+            MessageBox.Show(cbMevsim.Items[index].ToString());
+
+            switch (mevsim)
+            {
+                case "Kış": MessageBox.Show("Aralık\n Ocak\n Şubat\n"); break;
+                case "İlkbahar": MessageBox.Show("Mart\n Nisan\n Mayıs\n"); break;
+                case "Yaz": MessageBox.Show("Haziran\n Temmuz\n Ağustos\n"); break;
+                case "Sonbahar": MessageBox.Show("Eylül\n Ekim\n Kasım\n"); break;
+
+                default:
+                    MessageBox.Show("Hatalı seçim " );
+                    break;
+            }
+        }
+
+    }
+}
