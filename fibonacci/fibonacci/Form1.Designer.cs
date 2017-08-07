23bf48f4f5a7f6b21285225afecfccffd6a37276
Guven Bilgin <gvnblgn@hotmail.com>
2017-08-07 14:48:02 +0300

    fibonacci

---
+++ fibonacci/fibonacci/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace fibonacci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIndex = new System.Windows.Forms.TextBox();
+            this.lbIndex = new System.Windows.Forms.Label();
+            this.lbFibonacci = new System.Windows.Forms.ListBox();
+            this.btnIndex = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIndex
+            // 
+            this.txtIndex.Location = new System.Drawing.Point(186, 12);
+            this.txtIndex.Name = "txtIndex";
+            this.txtIndex.Size = new System.Drawing.Size(100, 20);
+            this.txtIndex.TabIndex = 0;
+            // 
+            // lbIndex
+            // 
+            this.lbIndex.AutoSize = true;
+            this.lbIndex.Location = new System.Drawing.Point(13, 12);
+            this.lbIndex.Name = "lbIndex";
+            this.lbIndex.Size = new System.Drawing.Size(161, 13);
+            this.lbIndex.TabIndex = 1;
+            this.lbIndex.Text = "Lutfen Fibonacci indexini Giriniz :";
+            // 
+            // lbFibonacci
+            // 
+            this.lbFibonacci.FormattingEnabled = true;
+            this.lbFibonacci.Location = new System.Drawing.Point(335, 12);
+            this.lbFibonacci.Name = "lbFibonacci";
+            this.lbFibonacci.Size = new System.Drawing.Size(231, 433);
+            this.lbFibonacci.TabIndex = 2;
+            // 
+            // btnIndex
+            // 
+            this.btnIndex.Location = new System.Drawing.Point(186, 77);
+            this.btnIndex.Name = "btnIndex";
+            this.btnIndex.Size = new System.Drawing.Size(100, 23);
+            this.btnIndex.TabIndex = 3;
+            this.btnIndex.Text = "Index i Bul";
+            this.btnIndex.UseVisualStyleBackColor = true;
+            this.btnIndex.Click += new System.EventHandler(this.btnIndex_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(186, 106);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Ekran Temizle";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 466);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnIndex);
+            this.Controls.Add(this.lbFibonacci);
+            this.Controls.Add(this.lbIndex);
+            this.Controls.Add(this.txtIndex);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIndex;
+        private System.Windows.Forms.Label lbIndex;
+        private System.Windows.Forms.ListBox lbFibonacci;
+        private System.Windows.Forms.Button btnIndex;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ fibonacci/fibonacci/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fibonacci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "Fibonacci Index";
+        }
+
+        private void btnIndex_Click(object sender, EventArgs e)
+        {
+            int sayi1 = 0;
+            int sayi2 = 1;
+            int toplam = 0;
+            int index = 0;
+            int sayac = 0;
+
+            lbFibonacci.ForeColor = Color.Green;
+            lbFibonacci.BackColor = Color.LightBlue;
+
+            index = Convert.ToInt32(txtIndex.Text);
+
+
+            for (int i = 1; i <= index; i++)
+            {
+                if (i == 1)
+                { 
+                    toplam = 0;
+                    lbFibonacci.Items.Add(toplam);
+                    sayac++;
+                }
+                else if (i==2)
+                {
+                    toplam = sayi1 + sayi2;
+                    lbFibonacci.Items.Add(toplam);
+                    sayac++;
+                }
+                else if(i>2)
+                {
+                    toplam = sayi1 + sayi2;
+                    lbFibonacci.Items.Add(toplam);
+                    sayi1 = sayi2;
+                    sayi2 = toplam;
+                    sayac++;
+                }
+
+            }//end of for
+            MessageBox.Show("Fibonacci sonucu : " + lbFibonacci.Items[sayac-1]);
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lbFibonacci.Items.Clear();
+            lbFibonacci.ForeColor = Color.Black;
+            lbFibonacci.BackColor = Color.White;
+        }
+    }
+}
