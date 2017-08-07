05782a999c53da481e5dd6f9c1b0ccc8e8e9bf10
guven.bilgin <gvnblgn@hotmail.com>
2017-08-07 20:09:24 +0300

    do while

---
+++ doWhile/doWhile/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace doWhile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.btnDoWhile = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Location = new System.Drawing.Point(26, 13);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(164, 52);
+            this.btnWhile.TabIndex = 0;
+            this.btnWhile.Text = "While";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // btnDoWhile
+            // 
+            this.btnDoWhile.Location = new System.Drawing.Point(26, 81);
+            this.btnDoWhile.Name = "btnDoWhile";
+            this.btnDoWhile.Size = new System.Drawing.Size(164, 52);
+            this.btnDoWhile.TabIndex = 1;
+            this.btnDoWhile.Text = "Do While";
+            this.btnDoWhile.UseVisualStyleBackColor = true;
+            this.btnDoWhile.Click += new System.EventHandler(this.btnDoWhile_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(239, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(249, 316);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 449);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnDoWhile);
+            this.Controls.Add(this.btnWhile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button btnDoWhile;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
