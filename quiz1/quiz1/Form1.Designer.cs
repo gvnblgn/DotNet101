bfd14f51b2ed49bbb3796691d97d6976ab9057ce
guven.bilgin <gvnblgn@hotmail.com>
2017-08-09 21:30:10 +0300

    quiz1 benim yaptığım

---
+++ quiz1/quiz1/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace quiz1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lbDisplay = new System.Windows.Forms.ListBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.btnSurname = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 33);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(96, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(267, 33);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(103, 20);
+            this.txtNo.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(390, 33);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(113, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Kaydet";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(12, 62);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(358, 20);
+            this.txtSearch.TabIndex = 4;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(390, 59);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(113, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Search Name";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lbDisplay
+            // 
+            this.lbDisplay.FormattingEnabled = true;
+            this.lbDisplay.Location = new System.Drawing.Point(12, 113);
+            this.lbDisplay.Name = "lbDisplay";
+            this.lbDisplay.Size = new System.Drawing.Size(358, 316);
+            this.lbDisplay.TabIndex = 8;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(141, 33);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(96, 20);
+            this.txtSurname.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 9;
+            this.lblName.Text = "Name";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(138, 9);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(49, 13);
+            this.lblSurname.TabIndex = 10;
+            this.lblSurname.Text = "Surname";
+            // 
+            // lblNo
+            // 
+            this.lblNo.AutoSize = true;
+            this.lblNo.Location = new System.Drawing.Point(264, 9);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Size = new System.Drawing.Size(75, 13);
+            this.lblNo.TabIndex = 11;
+            this.lblNo.Text = "Telephone No";
+            // 
+            // btnSurname
+            // 
+            this.btnSurname.Location = new System.Drawing.Point(390, 88);
+            this.btnSurname.Name = "btnSurname";
+            this.btnSurname.Size = new System.Drawing.Size(113, 23);
+            this.btnSurname.TabIndex = 6;
+            this.btnSurname.Text = "Search SurName";
+            this.btnSurname.UseVisualStyleBackColor = true;
+            this.btnSurname.Click += new System.EventHandler(this.btnSurname_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(390, 117);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(113, 23);
+            this.btnNo.TabIndex = 7;
+            this.btnNo.Text = "Search No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 441);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnSurname);
+            this.Controls.Add(this.lblNo);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.lbDisplay);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox lbDisplay;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblNo;
+        private System.Windows.Forms.Button btnSurname;
+        private System.Windows.Forms.Button btnNo;
+    }
+}
+
---
+++ quiz1/quiz1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quiz1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] name = new string[20];
+        string[] surname = new string[20];
+        string[] no = new string[20];
+
+        int i = 0;
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+
+
+            name[i] = txtName.Text.Trim();
+            surname[i] = txtSurname.Text.Trim();
+            no[i] = txtNo.Text.Trim();
+
+
+            txtName.Clear();
+            txtSurname.Clear();
+            txtNo.Clear();
+
+            //lbDisplay.Items.Add(name[i] +"       "  +surname[i]+ "            " + no[i]);
+            lbDisplay.Items.Add($"{name[i]} {surname[i]} {no[i]}");
+            i++;
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchName;
+            int buldum = 0;
+            searchName = txtSearch.Text;
+
+            for(int y=0; y<i; y++ )
+            {
+                if (searchName.Trim().ToLower() == name[y].ToLower())
+                {
+                    lbDisplay.SelectedIndex = y;
+                    buldum++;
+                }
+            }
+            if (buldum==0)
+            {
+                MessageBox.Show("Aradığınız isim data da bulunmamaktadır ! ");
+            }
+            txtSearch.Text = "";
+        }
+
+        private void btnSurname_Click(object sender, EventArgs e)
+        {
+            string searchSurname;
+            int buldum = 0;
+            searchSurname = txtSearch.Text;
+
+            for (int y = 0; y < i; y++)
+            {
+                if (searchSurname == surname[y])
+                {
+                    lbDisplay.SelectedIndex = y;
+                    buldum++;
+                }
+            }
+            if (buldum == 0)
+            {
+                MessageBox.Show("Aradığınız isim data da bulunmamaktadır ! ");
+            }
+            txtSearch.Text = "";
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            string numara;
+            int buldum = 0;
+            numara = txtSearch.Text;
+
+            for (int y = 0; y < i; y++)
+            {
+                if (numara == no[y])
+                {
+                    lbDisplay.SelectedIndex = y;
+                    buldum++;
+                }
+            }
+            if (buldum == 0)
+            {
+                MessageBox.Show("Aradığınız isim data da bulunmamaktadır ! ");
+            }
+            txtSearch.Text = "";
+        }
+    }
+}
