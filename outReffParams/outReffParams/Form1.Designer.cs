574ba013d21e04629c550451f7f0aa63aa738d3f
guven.bilgin <gvnblgn@hotmail.com>
2017-08-17 21:25:31 +0300

    params

---
+++ outReffParams/outReffParams/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace outReffParams
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnOut_2 = new System.Windows.Forms.Button();
+            this.btnOut_3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Out1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnOut_2
+            // 
+            this.btnOut_2.Location = new System.Drawing.Point(0, 91);
+            this.btnOut_2.Name = "btnOut_2";
+            this.btnOut_2.Size = new System.Drawing.Size(241, 85);
+            this.btnOut_2.TabIndex = 1;
+            this.btnOut_2.Text = "Out 2";
+            this.btnOut_2.UseVisualStyleBackColor = true;
+            this.btnOut_2.Click += new System.EventHandler(this.btnOut_2_Click);
+            // 
+            // btnOut_3
+            // 
+            this.btnOut_3.Location = new System.Drawing.Point(0, 182);
+            this.btnOut_3.Name = "btnOut_3";
+            this.btnOut_3.Size = new System.Drawing.Size(241, 85);
+            this.btnOut_3.TabIndex = 2;
+            this.btnOut_3.Text = "Out 3";
+            this.btnOut_3.UseVisualStyleBackColor = true;
+            this.btnOut_3.Click += new System.EventHandler(this.btnOut_3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 432);
+            this.Controls.Add(this.btnOut_3);
+            this.Controls.Add(this.btnOut_2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnOut_2;
+        private System.Windows.Forms.Button btnOut_3;
+    }
+}
+
---
+++ outReffParams/outReffParams/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace outReffParams
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void GeriyeDegerDon(int sayi, out double firlatilanSonuc) //herhangi bir return olmamasına rağmen out variable diğer taraftan yakalanabiliniyor.
+        {
+            firlatilanSonuc = Math.Pow(sayi, 2);
+        }
+
+        void Hesapla(int sayi1, int sayi2, out int toplam,out int cikarma, out long carpma, out double bolme, out byte mod)
+        {
+            toplam = sayi1 + sayi2;
+            cikarma = sayi1 - sayi2;
+            carpma = sayi1 * sayi2;
+            bolme = sayi1 / sayi2;
+            mod = (byte) (sayi1 % sayi2); 
+        }
+
+        double FiyatHesapla(params double[] fiyatlar)
+        {
+            double toplamFiyat = 0;
+            foreach (var item in fiyatlar)
+            {
+                toplamFiyat += item;
+            }
+            return toplamFiyat;
+        }
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            double yakanalanSonuc;
+            GeriyeDegerDon(12, out yakanalanSonuc);
+            MessageBox.Show(yakanalanSonuc.ToString()); // değişken return olmamasına rağmen burda yakalanıyor.
+        }
+
+        private void btnOut_2_Click(object sender, EventArgs e)
+        {
+            int toplam, cikarma;
+            long carpma;
+            double bolme;
+            byte mod;
+
+            Hesapla(8, 3, out toplam, out cikarma, out carpma, out bolme, out mod);
+        }
+
+        private void btnOut_3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(FiyatHesapla(193.20,56.55,68.74,123.54).ToString());
+        }
+    }
+}
