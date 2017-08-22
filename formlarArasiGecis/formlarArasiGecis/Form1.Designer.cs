6942c5a530358b612783bb746ac46a9d55bad4b2
guven.bilgin <gvnblgn@hotmail.com>
2017-08-22 19:56:51 +0300

    form geçişleri süper

---
+++ formlarArasiGecis/formlarArasiGecis/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace formlarArasiGecis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGonder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtGonder
+            // 
+            this.txtGonder.Location = new System.Drawing.Point(110, 12);
+            this.txtGonder.Name = "txtGonder";
+            this.txtGonder.Size = new System.Drawing.Size(100, 20);
+            this.txtGonder.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mesajınızı yazınız";
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Location = new System.Drawing.Point(110, 53);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(100, 23);
+            this.btnGonder.TabIndex = 2;
+            this.btnGonder.Text = "Mesaj";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.btnGonder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 390);
+            this.Controls.Add(this.btnGonder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGonder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGonder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGonder;
+    }
+}
+
---
+++ formlarArasiGecis/formlarArasiGecis/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formlarArasiGecis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Form2 frm2 = new Form2();
+
+        private void btnGonder_Click(object sender, EventArgs e)
+        {
+            frm2.mesaj = txtGonder.Text;
+            frm2.Show();
+            this.Hide();
+        }
+    }
+}
