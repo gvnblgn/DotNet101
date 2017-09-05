6f2d9549cbd485f6af9ce6c69d0453b985cf1b9a
guven.bilgin <gvnblgn@hotmail.com>
2017-09-05 21:33:53 +0300

    object oriented first project

---
+++ oOP_1/oOP_1/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace oOP_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPlaka = new System.Windows.Forms.TextBox();
+            this.txtMarka = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lbAraclar = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbRenk = new System.Windows.Forms.ComboBox();
+            this.numFiyat = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBoxEkle = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numFiyat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plaka Numarası:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Marka";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fiyat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Renk";
+            // 
+            // txtPlaka
+            // 
+            this.txtPlaka.Location = new System.Drawing.Point(123, 23);
+            this.txtPlaka.Name = "txtPlaka";
+            this.txtPlaka.Size = new System.Drawing.Size(100, 20);
+            this.txtPlaka.TabIndex = 4;
+            // 
+            // txtMarka
+            // 
+            this.txtMarka.Location = new System.Drawing.Point(123, 53);
+            this.txtMarka.Name = "txtMarka";
+            this.txtMarka.Size = new System.Drawing.Size(100, 20);
+            this.txtMarka.TabIndex = 5;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(123, 151);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(100, 23);
+            this.btnEkle.TabIndex = 8;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lbAraclar
+            // 
+            this.lbAraclar.FormattingEnabled = true;
+            this.lbAraclar.Location = new System.Drawing.Point(247, 42);
+            this.lbAraclar.Name = "lbAraclar";
+            this.lbAraclar.Size = new System.Drawing.Size(246, 238);
+            this.lbAraclar.TabIndex = 9;
+            this.lbAraclar.Click += new System.EventHandler(this.lbAraclar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxEkle);
+            this.groupBox1.Controls.Add(this.cbRenk);
+            this.groupBox1.Controls.Add(this.numFiyat);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtMarka);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtPlaka);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(229, 241);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Araç Ekleme Programı";
+            // 
+            // cbRenk
+            // 
+            this.cbRenk.FormattingEnabled = true;
+            this.cbRenk.Items.AddRange(new object[] {
+            "Black",
+            "White",
+            "Yellow",
+            "Brown",
+            "Orange",
+            "Red",
+            "Blue",
+            "Green"});
+            this.cbRenk.Location = new System.Drawing.Point(123, 116);
+            this.cbRenk.Name = "cbRenk";
+            this.cbRenk.Size = new System.Drawing.Size(100, 21);
+            this.cbRenk.TabIndex = 10;
+            // 
+            // numFiyat
+            // 
+            this.numFiyat.Location = new System.Drawing.Point(123, 83);
+            this.numFiyat.Maximum = new decimal(new int[] {
+            500000,
+            0,
+            0,
+            0});
+            this.numFiyat.Name = "numFiyat";
+            this.numFiyat.Size = new System.Drawing.Size(100, 20);
+            this.numFiyat.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(247, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(227, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Listeden Araç Silmek İçin Üstüne Çift Tıklayınız";
+            // 
+            // checkBoxEkle
+            // 
+            this.checkBoxEkle.AutoSize = true;
+            this.checkBoxEkle.Checked = true;
+            this.checkBoxEkle.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxEkle.Location = new System.Drawing.Point(9, 151);
+            this.checkBoxEkle.Name = "checkBoxEkle";
+            this.checkBoxEkle.Size = new System.Drawing.Size(47, 17);
+            this.checkBoxEkle.TabIndex = 12;
+            this.checkBoxEkle.Text = "Ekle";
+            this.checkBoxEkle.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 390);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbAraclar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numFiyat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPlaka;
+        private System.Windows.Forms.TextBox txtMarka;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lbAraclar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numFiyat;
+        private System.Windows.Forms.ComboBox cbRenk;
+        private System.Windows.Forms.CheckBox checkBoxEkle;
+    }
+}
+
---
+++ oOP_1/oOP_1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oOP_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Araba a1 = new Araba();
+            a1.plaka = txtPlaka.Text;
+            a1.marka = txtMarka.Text;
+            a1.fiyat = (double)numFiyat.Value;
+            a1.renk = cbRenk.SelectedItem.ToString();
+
+            lbAraclar.Items.Add(a1);
+            Temizle();
+        }
+
+
+
+        void Temizle()
+        {
+            txtMarka.Clear();
+            txtPlaka.Clear();
+            cbRenk.SelectedIndex = 0;
+            numFiyat.Value = 100;
+        }
+
+        private void lbAraclar_Click(object sender, EventArgs e)
+        {
+            Araba car = (Araba)lbAraclar.SelectedItem;
+            txtMarka.Text = car.marka;
+            txtPlaka.Text = car.plaka;
+            numFiyat.Value = (decimal)car.fiyat;
+            int index = cbRenk.Items.IndexOf(car.renk);
+            cbRenk.SelectedIndex = index;
+
+        }
+    }
+}
